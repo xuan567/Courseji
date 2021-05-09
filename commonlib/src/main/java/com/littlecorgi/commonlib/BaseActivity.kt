3d2782a71d54c38fe072726cde23cf3f657a3039
littlecorgi_twk <a1203991686@126.com>
2021-05-10 00:46:53 +0800

    移除友盟Push

--- commonlib/src/main/java/com/littlecorgi/commonlib/BaseActivity.kt
+++ commonlib/src/main/java/com/littlecorgi/commonlib/BaseActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.umeng.message.PushAgent
 import com.yanzhenjie.permission.Action
 import com.yanzhenjie.permission.AndPermission
 
@@ -14,7 +13,6 @@ open class BaseActivity() : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        PushAgent.getInstance(this).onAppStart()
     }
 
     /**
